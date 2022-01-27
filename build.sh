3a6bdcbc1e467083ace51e51400a5169ad37c81d
diamondburned <diamond@arikawa-hi.me>
2022-01-27 15:33:50 -0800

    Add 01

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+flagfile() {
+	tr $'\n' ' ' < "$PROJECT_ROOT/$1"
+}
+
+executableFileName=$(basename "$PWD")
+sourceFiles=( $(find ./ -path ./.\* -prune -o -name "*.cpp" -print) )
+
+g++ \
+	$(flagfile compile_flags_g++.txt) \
+	-o "${executableFileName}_g++" "${sourceFiles[@]}"
+
+clang++ \
+	$(flagfile compile_flags.txt) \
+	-o "${executableFileName}_clang++" "${sourceFiles[@]}"
+
